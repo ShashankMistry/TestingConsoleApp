99a9951866b52cf0da3122b92aa8d99fc4b8053d
Shashank Mistry <shashankmistry30@gmail.com>
2023-02-08 20:57:35 -0500

    console app

--- Assignment2/Circle/Program.cs
+++ Assignment2/Circle/Program.cs
@@ -10,8 +10,35 @@ namespace Assignment2_SM_MP
     {
         static void Main(string[] args)
         {
-            // Display the number of command line arguments.
-            Console.WriteLine(args.Length);
+            Circle circle = new Circle(); 
+            while (true) {
+                Console.WriteLine("1. Get Circle Radius");
+                Console.WriteLine("2. Change Circle Radius");
+                Console.WriteLine("3. Get Circle Circumference");
+                Console.WriteLine("4. Get Circle Area");
+                Console.WriteLine("5. Exit");
+                //
+                int selection = Convert.ToInt16(Console.ReadLine());
+
+                if (selection == 1) {
+                    Console.WriteLine("Radius: "+circle.GetRadius());
+                    
+                } else if (selection == 2)
+                {
+                    Console.WriteLine("Set Radius: ");
+                    int radius = Convert.ToInt16(Console.ReadLine());
+                    circle.SetRadius(radius);
+                   
+                } else if (selection == 3)
+                {
+                    Console.WriteLine("Circumference of Circle is: "+ circle.GetCircumference());
+                } else if (selection == 4)
+                {
+                    Console.WriteLine("Area of Circle: "+ circle.GetArea());
+                } else if (selection >= 5) {
+                    Environment.Exit(1);
+                }
+            }
         }
     }
 }
